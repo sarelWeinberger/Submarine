8fd17af606689b06658093ddc1d77b8bf522765d
Sarel Weinberger <sarelw1@gmail.com>
2020-04-02 03:37:27 +0300

    whith no nested calsses

--- bord.py
+++ bord.py
@@ -18,6 +18,7 @@ class Bord:
     COLUMN_SIZE = 10
     cells = np.arange(ROW_SIZE, COLUMN_SIZE)
 
+    @staticmethod
     def check_input(input_index, filling_bord=False, hitting_competitor=False):
         """checking if the input is relevant to the game"""
         while True:
@@ -38,6 +39,7 @@ class Bord:
         # def check_filling(in_index):
         #     pass
 
+    @staticmethod
     def length_check(submarine_size, submarine_start, submarine_end):
         # checking length
         while True:
@@ -45,16 +47,16 @@ class Bord:
                 assert ((submarine_end - submarine_start) == submarine_size)
                 return submarine_start, submarine_end
             except:
-                print(Bcolors.FAIL +"wrong size" + Bcolors.ENDC)
+                print(Bcolors.FAIL + "wrong size" + Bcolors.ENDC)
                 submarine_start = raw_input(" try anther parameter set new start point")
                 submarine_start = Bord.check_input(submarine_start)
                 submarine_end = raw_input(" try anther parameter set new end point")
                 submarine_end = Bord.check_input(submarine_end)
-                return submarine_start, submarine_end
                 continue
             else:
                 break
 
+    @staticmethod
     def orientation_validation(submarine_orientation):
         submarine_orientation = submarine_orientation.upper()
         while True:
@@ -66,7 +68,45 @@ class Bord:
                 submarine_orientation = raw_input(Bcolors.OKBLUE +
                                                   'choose "V" to vertical submarine or "H" to horizontal submarine' + Bcolors.ENDC)
                 submarine_orientation = submarine_orientation.upper()
-                return submarine_orientation
                 continue
             else:
                 break
+
+    @staticmethod
+    def collision_or_snap_check(cells, row_x, row_y, orientation, first_iteration):
+        # collision
+        filling_successful = False
+        try:
+            assert (cells[row_x, row_y] == 0)
+            filling_successful = True
+        except:
+            print(Bcolors.FAIL + "cell already occupied try new position{} {}".format('\n', cells) + Bcolors.ENDC)
+
+        if filling_successful:
+            # snap:
+            try:
+                assert (cells[row_x + 1, row_y + 1] == 0)
+                assert (cells[row_x + 0, row_y + 1] == 0)
+                assert (cells[row_x + 1, row_y + 0] == 0)
+
+                assert (cells[row_x - 1, row_y - 1] == 0)
+
+                assert (cells[row_x - 1, row_y + 1] == 0)
+                assert (cells[row_x + 1, row_y - 1] == 0)
+
+                # print(MyBord.my_cells)
+                if first_iteration:
+                    assert (cells[row_x - 0, row_y - 1] == 0)
+                    assert (cells[row_x - 1, row_y - 0] == 0)
+                elif orientation == 'V' and first_iteration == False:
+                    assert (cells[row_x - 0, row_y - 1] == 0)
+                elif orientation == 'H' and first_iteration == False:
+                    assert (cells[row_x - 1, row_y - 0] == 0)
+
+                filling_successful = True
+            except:
+                print(
+                    Bcolors.FAIL + "The cell is adjacent to an existing submarine {} {}".format('\n', cells) + Bcolors.ENDC)
+                filling_successful = False
+
+        return filling_successful
---
+++ submarine.py
@@ -0,0 +1,55 @@
+from bord import Bcolors
+from notebook.notebookapp import raw_input
+from bord import Bord
+
+
+class Submarine:
+
+    def __init__(self, submarine_len, sub_name):
+        self.checking_ok = False
+        self.submarine_length = submarine_len
+        self.submarine_name = sub_name
+
+    def fill_submarine_request(self):
+
+        # 1. orientation
+        self.submarine_orientation = raw_input(
+            Bcolors.OKBLUE + 'choose "V" to vertical submarine or "H" to horizontal ' + self.submarine_name + Bcolors.ENDC)
+        self.submarine_orientation = Bord.orientation_validation(self.submarine_orientation)
+
+        # 2. define stat point and end point
+        # a. row start:
+        self.submarine_row_start = raw_input(Bcolors.OKGREEN +
+                                             'choose row for the {} size submarine starting point - the small number in the index'.format(
+                                                 self.submarine_name) + Bcolors.ENDC)
+        self.submarine_row_start = Bord.check_input(self.submarine_row_start)
+
+        # b. row end
+        if self.submarine_orientation == 'V':
+            self.submarine_row_end = raw_input(Bcolors.OKGREEN +
+                                               'choose row for the {} size submarine end point - the big number in the index'.format(
+                                                   self.submarine_name) + Bcolors.ENDC)
+            self.submarine_row_end = Bord.check_input(self.submarine_row_end)
+            submarine_row_start, submarine_row_end = Bord.length_check(self.submarine_length,
+                                                                       self.submarine_row_start, self.submarine_row_end)
+
+        # c. column start
+        self.submarine_column_start = raw_input(
+            'choose column for the {} size submarine start point - the small number in the index'.format(
+                self.submarine_name))
+        self.submarine_column_start = Bord.check_input(self.submarine_column_start)
+
+        # d. column end
+        if self.submarine_orientation == 'H':
+            self.submarine_column_end = raw_input(
+                'choose column for the {} size submarine end point - the big number in the index'.format(
+                    self.submarine_name))
+            self.submarine_column_end = Bord.check_input(self.submarine_column_end)
+            submarine_column_start, submarine_column_end = Bord.length_check(self.submarine_length,
+                                                                             self.submarine_column_start,
+                                                                             self.submarine_column_end)
+
+
+
+
+
