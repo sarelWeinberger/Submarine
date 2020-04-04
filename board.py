00c2c85a227c91d4eac91276122927ddbe3e5788
Sarel Weinberger <sarelw1@gmail.com>
2020-04-05 00:48:11 +0300

    after ofek first comments

--- board.py
+++ board.py
@@ -13,47 +13,49 @@ class Bcolors:
     UNDERLINE = '\033[4m'
 
 
-class Bord:
+class Board:
     ROW_SIZE = 10
     COLUMN_SIZE = 10
 
-    cells = np.arange(ROW_SIZE, COLUMN_SIZE)
-
     @staticmethod
-    def check_input(input_index,hitting_competitor=False):
+    def check_input_define_in_the_board(input_index, hitting_competitor=False):
         """checking if the input is relevant to the game"""
+        new_input = None
         while True:
             try:
-                in_index = int(input_index)
-                assert (int(in_index) > 0 and (int(in_index) <= Bord.ROW_SIZE))
+                if new_input is None:
+                    in_index = int(input_index)
+                else:
+                    in_index = int(new_input)
+                assert (int(in_index) > 0 and (int(in_index) <= Board.ROW_SIZE))
                 return in_index
             except Exception as e:
-                print(Bcolors.FAIL + str(e.args) + "Incorrect input, not an int between 0 and {}".format(Bord.ROW_SIZE)  + Bcolors.ENDC)
+                print(Bcolors.FAIL + str(e.args) + "Incorrect input, not an int between 0 and {}".format(
+                    Board.ROW_SIZE) + Bcolors.ENDC)
                 new_input = raw_input('try new input')
-                return int(new_input)
                 continue
             else:
                 break
 
     @staticmethod
-    def length_check(submarine_size, submarine_start, submarine_end):
+    def submarine_legal_length_check(submarine_size, submarine_start, submarine_end):
         # checking length
         while True:
             try:
                 assert ((submarine_end - submarine_start) == submarine_size)
                 return submarine_start, submarine_end
             except:
-                print(Bcolors.FAIL + "wrong size" + Bcolors.ENDC)
+                print(Bcolors.FAIL + "wrong submarine size should be size of {}".format(submarine_size) + Bcolors.ENDC)
                 submarine_start = raw_input(" try anther parameter set new start point")
-                submarine_start = Bord.check_input(submarine_start)
+                submarine_start = Board.check_input_define_in_the_board(submarine_start)
                 submarine_end = raw_input(" try anther parameter set new end point")
-                submarine_end = Bord.check_input(submarine_end)
+                submarine_end = Board.check_input_define_in_the_board(submarine_end)
                 continue
             else:
                 break
 
     @staticmethod
-    def orientation_validation(submarine_orientation):
+    def submarine_orientation_input_validation(submarine_orientation):
         submarine_orientation = submarine_orientation.upper()
         while True:
             try:
@@ -69,17 +71,17 @@ class Bord:
                 break
 
     @staticmethod
-    def collision_or_snap_check(cells, row_x, row_y, orientation):
-        # collision
+    def checking_cells_occupied_or_adjacent_to_existing_submarine(cells, row_x, row_y, orientation):
+        # occupied_checking
         filling_successful = False
         try:
             assert (cells[row_x, row_y] == 0)
             filling_successful = True
-        except:
-            print(Bcolors.FAIL + "cell already occupied try new position{} {}".format('\n', cells) + Bcolors.ENDC)
+        except Exception as e:
+            print(Bcolors.FAIL + "cell already occupied try new position{} {}, {}".format('\n', cells, e.args) + Bcolors.ENDC)
 
         if filling_successful:
-            # snap:
+            # adjacent_checking:
             try:
                 assert (cells[row_x + 1, row_y + 1] == 0)
                 assert (cells[row_x + 0, row_y + 1] == 0)
@@ -94,9 +96,10 @@ class Bord:
                 assert (cells[row_x - 1, row_y - 0] == 0)
 
                 filling_successful = True
-            except:
+            except Exception as e:
                 print(
-                    Bcolors.FAIL + "The cell is adjacent to an existing submarine {} {}".format('\n', cells) + Bcolors.ENDC)
+                    Bcolors.FAIL + "The cell is adjacent to an existing submarine {} {}, {}".format('\n',
+                                                                                                cells, e.args) + Bcolors.ENDC)
                 filling_successful = False
 
         return filling_successful
--- myborad.py
+++ myborad.py
@@ -1,39 +1,40 @@
 import numpy as np
-from bord import Bord
+from board import Board
 from submarine import Submarine
 
 
-class MyBord():
+class MyBorad():
     # generals
-    my_cells = np.zeros((Bord.ROW_SIZE + 1, Bord.COLUMN_SIZE + 1), dtype=int)
-    for i in range(Bord.ROW_SIZE + 1):
+    my_cells = np.zeros((Board.ROW_SIZE + 1, Board.COLUMN_SIZE + 1), dtype=int)
+    for i in range(Board.ROW_SIZE + 1):
         my_cells[i, 0] = i
         my_cells[0, i] = i
 
     def __init__(self):
 
-        # TODO: add some condition - when we want the user to fill the bord!!!
+        # TODO: add some condition - when we want the user to fill the board!!!
         for i in range(5, 1, -1):
             submarine_name = 'submarine_' + str(i)
-            print("fill {} length , current bord: {}, rows \ columns {} {}".format(submarine_name,'\n', '\n',self.my_cells))
+            print("fill {} length , current board: {}, rows \ columns {} {}".format(submarine_name,'\n', '\n',self.my_cells))
             submarine_name = Submarine(i, submarine_name)
             submarine_name.fill_submarine_request()
-            MyBord.checking_before_filling(submarine_name)
+            MyBorad.checking_before_filling(submarine_name)
             if submarine_name.checking_ok:
-                MyBord.position_the_submarine(submarine_name)
+                MyBorad.position_the_submarine(submarine_name)
 
-        print("my cells full:  '\n' {} ".format(MyBord.my_cells))
+        print("my cells full:  '\n' {} ".format(MyBorad.my_cells))
 
     @classmethod
     def checking_before_filling(cls, submarine_name):
+        filling_successful = False
         if submarine_name.submarine_orientation == 'H':
             # checking
             for i in range(submarine_name.submarine_column_start, submarine_name.submarine_column_end):
-                filling_successful = Bord.collision_or_snap_check(cls.my_cells, submarine_name.submarine_row_start, i,
-                                                                  submarine_name.submarine_orientation)
+                filling_successful = Board.checking_cells_occupied_or_adjacent_to_existing_submarine(cls.my_cells, submarine_name.submarine_row_start, i,
+                                                                                                     submarine_name.submarine_orientation)
                 while not filling_successful:
                     submarine_name.fill_submarine_request()
-                    filling_successful = MyBord.checking_before_filling(submarine_name)
+                    filling_successful = MyBorad.checking_before_filling(submarine_name)
                     if filling_successful:
                         submarine_name.checking_ok = True
                         return filling_successful
@@ -43,11 +44,11 @@ class MyBord():
         if submarine_name.submarine_orientation == 'V':
             # checking
             for i in range(submarine_name.submarine_row_start, submarine_name.submarine_row_end):
-                filling_successful = Bord.collision_or_snap_check(cls.my_cells, i, submarine_name.submarine_column_start,
-                                                                  submarine_name.submarine_orientation)
+                filling_successful = Board.checking_cells_occupied_or_adjacent_to_existing_submarine(cls.my_cells, i, submarine_name.submarine_column_start,
+                                                                                                     submarine_name.submarine_orientation)
                 while not filling_successful:
                     submarine_name.fill_submarine_request()
-                    filling_successful = MyBord.checking_before_filling(submarine_name)
+                    filling_successful = MyBorad.checking_before_filling(submarine_name)
                     if filling_successful:
                         submarine_name.checking_ok = True
                         return filling_successful
